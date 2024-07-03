6f135def0b830f4213d4b707f7a5dbc0d3a18e58
牛道 128 <121217935+ushimichi@users.noreply.github.com>
2024-07-03 15:32:06 +0000

    得点要素等追加

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let score = 0;
+let scoreText;
+let gameOverText;
+let touchX = null;
+let isGameOver = false;
+
 function preload() {
     this.load.image('background', 'imgs/background.png');
     this.load.image('player', 'imgs/player.png');
@@ -55,14 +61,45 @@ function create() {
 
     // 衝突判定
     this.physics.add.overlap(this.bullets, this.enemies, destroyEnemy, null, this);
+    this.physics.add.overlap(this.player, this.enemies, gameOver, null, this);
+
+    // スコア表示
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+
+    // ゲームオーバー表示
+    gameOverText = this.add.text(400, 300, '', { fontSize: '64px', fill: '#fff' });
+    gameOverText.setOrigin(0.5);
+    gameOverText.setVisible(false);
+
+    // タッチイベントの設定
+    this.input.on('pointermove', function (pointer) {
+        touchX = pointer.x;
+    });
+
+    this.input.on('pointerdown', function (pointer) {
+        if (isGameOver) {
+            restartGame.call(this);
+        } else {
+            fireBullet.call(this);
+        }
+    }, this);
 }
 
 function update() {
+    if (isGameOver) return;
+
     // プレイヤーの移動
     if (this.cursors.left.isDown) {
         this.player.setVelocityX(-200);
     } else if (this.cursors.right.isDown) {
         this.player.setVelocityX(200);
+    } else if (touchX !== null) {
+        const diff = touchX - this.player.x;
+        if (Math.abs(diff) > 10) {
+            this.player.setVelocityX(diff);
+        } else {
+            this.player.setVelocityX(0);
+        }
     } else {
         this.player.setVelocityX(0);
     }
@@ -84,6 +121,8 @@ function fireBullet() {
 }
 
 function addEnemy() {
+    if (isGameOver) return;
+
     const x = Phaser.Math.Between(50, 750);
     const enemy = this.enemies.create(x, 0, 'enemy');
     enemy.setVelocityY(100);
@@ -92,4 +131,25 @@ function addEnemy() {
 function destroyEnemy(bullet, enemy) {
     bullet.destroy();
     enemy.destroy();
+    score += 10;
+    scoreText.setText('Score: ' + score);
+}
+
+function gameOver(player, enemy) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    enemy.destroy();
+    isGameOver = true;
+    gameOverText.setText('Game Over! Tap to Restart');
+    gameOverText.setVisible(true);
+}
+
+function restartGame() {
+    isGameOver = false;
+    this.physics.resume();
+    this.player.clearTint();
+    this.enemies.clear(true, true);
+    score = 0;
+    scoreText.setText('Score: ' + score);
+    gameOverText.setVisible(false);
 }
